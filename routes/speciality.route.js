466b0ed92dff191dc0f47113510c16b217fb4616
Chirag Lathiya <chirag@greymatterinnovationz.com>
2025-02-28 19:05:32 +0530

    speciality route add

--- routes/speciality.route.js
+++ routes/speciality.route.js
@@ -6,7 +6,7 @@ import { singleUpload } from "../middleware/multer.js";
 const router = express.Router();
 
 router.route("/addSpeciality").post( addSpeciality);
-router.route("/getSpecialities/:id").get( getSpecialities);
+router.route("/getSpecialities").get( getSpecialities);
 router.route("/getSpecialityById/:id").put( getSpecialityById);
 router.route("/updateSpeciality/:id").put( updateSpeciality);
 router.route("/deleteSpeciality/:id").delete(deleteSpeciality);

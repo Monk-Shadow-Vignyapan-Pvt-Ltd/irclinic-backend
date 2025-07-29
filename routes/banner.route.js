813dfe162096012bb0bfb431128dd6af63e65a6a
Chirag Lathiya <chirag@greymatterinnovationz.com>
2025-07-29 17:54:29 +0530

    Fix Url Banner

--- routes/banner.route.js
+++ routes/banner.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addBanner, getBanners,getMobileBanners,getDesktopBanners, getBannerById, deleteBanner, updateBanner} from "../controllers/banner.controller.js";
+import { addBanner, getBanners,getMobileBanners,getDesktopBanners,getMobileAboutBanners,getDesktopAboutBanners, getBannerById, deleteBanner, updateBanner} from "../controllers/banner.controller.js";
 import {auth} from "../middleware/auth.js"
 import isAuthenticated from "../auth/isAuthenticated.js";
 import { singleUpload } from "../middleware/multer.js";
@@ -10,6 +10,8 @@ router.route("/addBanner").post( addBanner);
 router.route("/getBanners").get( getBanners);
 router.route("/getMobileBanners").get( getMobileBanners);
 router.route("/getDesktopBanners").get( getDesktopBanners);
+router.route("/getMobileAboutBanners").get( getMobileAboutBanners);
+router.route("/getDesktopAboutBanners").get( getDesktopAboutBanners);
 router.route("/getBannerById/:id").put( getBannerById);
 router.route("/updateBanner/:id").post( updateBanner);
 router.route("/deleteBanner/:id").delete(deleteBanner);

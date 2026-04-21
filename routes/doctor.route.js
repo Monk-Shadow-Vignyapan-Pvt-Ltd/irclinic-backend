fc3d97d8ba28a7537c110ea2010720c78a9ac612
Lcdtech <clathiya403@gmail.com>
2026-04-21 12:57:37 +0530

    doctor api fix

--- routes/doctor.route.js
+++ routes/doctor.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addDoctor, getDoctors,getAllDoctors,getReferenceDoctors,
+import { addDoctor, getDoctors,getAllDoctors,getAllDoctorsByCenterId,getReferenceDoctors,
      getDoctorById, deleteDoctor, updateDoctor,dashboardDoctors,searchDoctors, getDoctorsExcel} from "../controllers/doctor.controller.js";
 import isAuthenticated from "../auth/isAuthenticated.js";
 import { singleUpload } from "../middleware/multer.js";
@@ -9,6 +9,7 @@ const router = express.Router();
 router.route("/addDoctor").post( addDoctor);
 router.route("/getDoctors/:id").get( getDoctors);
 router.route("/getAllDoctors").get( getAllDoctors);
+router.route("/getAllDoctorsByCenterId/:id").get(getAllDoctorsByCenterId);
 router.route("/getReferenceDoctors/:id").get( getReferenceDoctors);
 router.route("/getDoctorById/:id").put( getDoctorById);
 router.route("/updateDoctor/:id").put( updateDoctor);
